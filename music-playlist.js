19ffb63edfa1b8e9fd04627aefdb2fcabefda32d
unreal! <aimaressi88@gmail.com>
2026-03-20 03:59:47 +0100

    Update playlist name and add new tracks

--- music-playlist.js
+++ music-playlist.js
@@ -2,10 +2,19 @@ let globalIdx = 0;
 
 const songs = [
     {
-        name: "test",
-        description: "test songs",
+        name: "unreal's playlist",
+        description: "",
         tracks: [
-            { title: "Mario Kart Wii - Title Theme", src: "https://itzmeunreal.github.io/music/mario_kart_wii_title_theme.mp3", meta: "Nintendo", globalIndex: globalIdx++ },
+            { title: "it's always a good time", src: "https://itzmeunreal.github.io/music/it's always a good time.mp3", meta: "", globalIndex: globalIdx++ },
+            { title: "mashups aren't real music imagine making them", src: "https://itzmeunreal.github.io/music/mashups aren't real music imagine making them.mp3", meta: "", globalIndex: globalIdx++ },
+            { title: "the elon musk soundcloud acquisition", src: "https://itzmeunreal.github.io/music/the elon musk soundcloud acquisition.mp3", meta: "", globalIndex: globalIdx++ },
+            { title: "we respawn", src: "https://itzmeunreal.github.io/music/we respawn.mp3", meta: "", globalIndex: globalIdx++ },
+            { title: "insane anime obsessed boyfriend", src: "https://itzmeunreal.github.io/music/insane anime obsessed boyfriend.mp3", meta: "", globalIndex: globalIdx++ },
+            { title: "WLD U HOLD IT AGAINST ME TURN.UP.THE.BASS Part 117", src: "https://itzmeunreal.github.io/music/WLD U HOLD IT AGAINST ME_ TURN.UP.THE.BASS -Part_117.mp3", meta: "", globalIndex: globalIdx++ },
+            { title: "toby fox discovers the hyperpop soundfont", src: "https://itzmeunreal.github.io/music/toby fox discovers the hyperpop soundfont.mp3", meta: "", globalIndex: globalIdx++ },
+            { title: "it's always a good time for complextro", src: "https://itzmeunreal.github.io/music/it's always a good time for complextro.mp3", meta: "", globalIndex: globalIdx++ },
+            { title: "TAKE-ME-AWAY ~so why try to fight it~", src: "https://itzmeunreal.github.io/music/TAKE-ME-AWAY ~so why try to fight it_~.mp3", meta: "", globalIndex: globalIdx++ },
+            { title: "leroy snares will soon replace NFTs", src: "https://itzmeunreal.github.io/music/leroy snares will soon replace NFTs.mp3", meta: "", globalIndex: globalIdx++ },
         ]
     },
 ];
