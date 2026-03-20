ce5b5f720dcb4c640592e2ff462186fa4e60d4bb
unreal! <aimaressi88@gmail.com>
2026-03-20 03:47:52 +0100

    Update music source URL for Mario Kart Wii theme

--- music-playlist.js
+++ music-playlist.js
@@ -5,7 +5,7 @@ const songs = [
         name: "test",
         description: "test songs",
         tracks: [
-            { title: "Mario Kart Wii - Title Theme", src: "https://frutigeraeroarchive.org/music/mario_kart_wii_title_theme.mp3", meta: "Nintendo", globalIndex: globalIdx++ },
+            { title: "Mario Kart Wii - Title Theme", src: "https://itzmeunreal.github.io/music/mario_kart_wii_title_theme.mp3", meta: "Nintendo", globalIndex: globalIdx++ },
         ]
     },
 ];
