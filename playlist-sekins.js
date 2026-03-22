351b7aeaede0f64ee22e47b6915bfec922fbfe67
unreal! <aimaressi88@gmail.com>
2026-03-23 00:41:33 +0100

    Create playlist-sekins.js

---
+++ playlist-sekins.js
@@ -0,0 +1,20 @@
+window.playlistData = {
+    name: "sekins",
+    pfp: "https://itzmeunreal.neocities.org/images/covers/sekins/sekins.jpg",
+    description: "sekins one of the best artist i've ever met and became friends with. they haven't finished a lot of songs but they still post some wips on their <a href='https://dope1kitty.nekoweb.org/'>socials</a>, make sure to follow them.",
+    sectionName: "sekins",
+    tracks: [
+        {
+            title: "serum soda",
+            src: "https://file.garden/abIGtIH1yApu4jf7/songs/sekins/serum_soda.mp3",
+            meta: "sekins",
+            cover: "https://itzmeunreal.neocities.org/images/covers/sekins/serumsoda.jpg"
+        },
+        {
+            title: "vst3",
+            src: "https://file.garden/abIGtIH1yApu4jf7/songs/sekins/vst3.mp3",
+            meta: "sekins",
+            cover: "https://itzmeunreal.neocities.org/images/covers/sekins/vst3.jpg"
+        },
+    ]
+};
