9f09ec8835a5477dafae0fc4724bef700c566988
unreal! <aimaressi88@gmail.com>
2026-04-03 20:29:50 +0200

    Change playlist file to 'playlist-sekins.js'

--- music.js
+++ music.js
@@ -217,4 +217,4 @@ document.querySelectorAll('.mp-artist-grid-item').forEach(item => {
 });
 
 volumeBar.style.setProperty('--vol', '70%');
-loadPlaylistFile('playlist-xaev.js').then(data => applyPlaylist(data));
+loadPlaylistFile('playlist-sekins.js').then(data => applyPlaylist(data));
